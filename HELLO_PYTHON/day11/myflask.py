45e0b2f4423ad00efe0a516362362a6e8c391b38
Jangyeji <crush03203@naver.com>
2022-12-07 22:01:17 +0900

    12.07

--- HELLO_PYTHON/day11/myflask.py
+++ HELLO_PYTHON/day11/myflask.py
@@ -1,16 +1,19 @@
 from day11.dao_mem import DaoMem
-from flask.templating import render_template
-from flask.app import Flask
+from flask import Flask
+from flask import request
+import psycopg2
+from flask import Flask,render_template
 
 app = Flask(__name__)
 #list화면 출력
 
-
+@app.route('/')
 @app.route('/mem_list')
 def mem_list():
     ds = DaoMem()
     list = ds.selects()
     return render_template('mem_list.html', list=list)
-    
+
+
 if __name__ == '__main__':
     app.run(debug=True)
